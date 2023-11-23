a9ce135ca4136d49a073faacaeb128e085028c7c
alexeykazancev <kazual21@gmail.com>
2023-11-23 21:32:05 +0300

    add

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "vm_data" {
+output "vm_data0" {
   value = ["${proxmox_vm_qemu.vm0.*.name}", "${proxmox_vm_qemu.vm0.*.ssh_host}"]
 }
 
-output "vm_data" {
+output "vm_data1" {
   value = ["${proxmox_vm_qemu.vm1.*.name}", "${proxmox_vm_qemu.vm1.*.ssh_host}"]
 }
