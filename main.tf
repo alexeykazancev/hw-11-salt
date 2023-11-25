10bfadd1b446f7af1e37249c4929c29a8860531b
alexeykazancev <kazual21@gmail.com>
2023-11-25 20:30:36 +0300

    add

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "proxmox_vm_qemu" "salt-server" {
       "echo deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://repo.saltproject.io/salt/py3/debian/11/amd64/latest bullseye main | tee /etc/apt/sources.list.d/salt.list",
       "apt update",
       "apt install -y salt-master salt-ssh salt-syndic salt-cloud salt-api",
-      "echo interface: 10.128.64.162 | tee -a /etc/salt/master",
+      #"echo interface: 10.128.64.162 | tee -a /etc/salt/master",
       "systemctl enable salt-master && systemctl start salt-master",
       "systemctl enable salt-syndic && systemctl start salt-syndic",
       "systemctl enable salt-api && systemctl start salt-api",
@@ -150,7 +150,7 @@ resource "proxmox_vm_qemu" "salt-minion" {
       "echo deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://repo.saltproject.io/salt/py3/debian/11/amd64/latest bullseye main | tee /etc/apt/sources.list.d/salt.list",
       "apt update",
       "apt install -y salt-minion salt-ssh",
-      "echo minion-0 | tee -a /etc/salt/minion_id",
+      #"echo minion-0 | tee -a /etc/salt/minion_id",
       "systemctl enable salt-minion && systemctl start salt-minion",
     ]
   }
